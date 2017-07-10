7f1a4a7e8807161555de0beb63d1a3ec8a7f1085
BarkaBoss <bossmajor.biii@gmail.com>
2017-07-10 10:02:30 +0100

    Corrections

--- src/cmp202/ExecuteFormulas.java
+++ src/cmp202/ExecuteFormulas.java
@@ -11,16 +11,34 @@ package cmp202;
  *
  * @author Barka
  */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import javax.swing.*;
+
 public class ExecuteFormulas {
     
     public static void main(String [] args)
     {
         Formulas myFormula = new Formulas();
         
+        Scanner sc = new Scanner(System.in);
+        JOptionPane.showInputDialog("Enter X");
+        
+        try{
+        float distance = sc.nextFloat();
+        float force = sc.nextFloat();
+        
         float myKinetic = myFormula.calcKineticEnergy(34.5f, 45.78f);
-        float work = myFormula.calculateWork(34.5f, 45.78f);
+        float work = myFormula.calculateWork(distance, force);
         
         System.out.println(work);
         System.out.println(myKinetic);
+        }
+        catch(InputMismatchException iME)
+        {
+            JOptionPane.showMessageDialog(null, "Wrong input type");
+                    
+        }
     }
 }
---
+++ src/cmp202/JOp.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cmp202;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Barka
+ */
+public class JOp {
+    public static void main(String [] args)
+    {
+        JOptionPane.showMessageDialog(null, "Hello Class");
+    }
+}
